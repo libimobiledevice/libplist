13ecca44eefd367354070a22dec7c0829e1ba6fd
Nikias Bassen <nikias@gmx.li>
2011-09-11 01:03:09 +0200

    Win32: do not use visibility attribute

--- src/common.h
+++ src/common.h
@@ -4,7 +4,7 @@
 #define PLIST_LITTLE_ENDIAN 0
 #define PLIST_BIG_ENDIAN 1
 
-#if defined(__GNUC__) && (__GNUC__ >= 4) && !defined(__CYGWIN__)
+#if defined(__GNUC__) && (__GNUC__ >= 4) && !defined(__CYGWIN__) && !defined(WIN32)
 # define _PLIST_INTERNAL      __attribute__((visibility("hidden")))
 #elif defined(__SUNPRO_C) && (__SUNPRO_C >= 0x550)
 # define _PLIST_INTERNAL      __hidden
