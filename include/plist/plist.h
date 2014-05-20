135164ae0676ccd2f5cbb9f88d4ea4bd35d1a584
Nikias Bassen <nikias@gmx.li>
2014-05-20 23:43:36 +0200

    Move deprecation marker after the function declaration for better compiler output

--- include/plist/plist.h
+++ include/plist/plist.h
@@ -361,7 +361,7 @@ extern "C"
      * @param item the new item to insert
      * @param key The identifier of the item to insert.
      */
-    PLIST_WARN_DEPRECATED("use plist_dict_set_item instead") PLIST_API void plist_dict_insert_item(plist_t node, const char* key, plist_t item);
+    PLIST_API void plist_dict_insert_item(plist_t node, const char* key, plist_t item) PLIST_WARN_DEPRECATED("use plist_dict_set_item instead");
 
     /**
      * Remove an existing position in a #PLIST_DICT node.
