84566379a187dda1fe19199e0d8f4910a1a7bb62
Nikias Bassen <nikias@gmx.li>
2014-05-22 20:26:02 +0200

    xplist: Handle DST-affected timezones correctly

--- src/xplist.c
+++ src/xplist.c
@@ -323,7 +323,7 @@ static void parse_date(const char *strval, struct tm *btime)
     btime->tm_year-=1900;
     btime->tm_mon--;
 #endif
-    btime->tm_isdst=-1;
+    btime->tm_isdst=0;
 }
 
 static void xml_to_node(xmlNodePtr xml_node, plist_t * plist_node)
