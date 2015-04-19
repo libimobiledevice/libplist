a7f3ee21f146f762f579e96bd3085df3dc2dfc49
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-04-19 15:46:47 +0200

    Visual C++ Compatibility: Configuration headers
    Add msc_config.h file which contains definitions for inline, __func__ and various definitions that make the Visual C++ compiler be compatible with the libplist codebase

--- src/xplist.c
+++ src/xplist.c
@@ -21,6 +21,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#ifdef _MSC_VER
+#include "msc_config.h"
+#endif
 
 #include <string.h>
 #include <assert.h>
