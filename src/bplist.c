4c072d015193af74719d07897c0a0b4396b4a866
Nikias Bassen <nikias@gmx.li>
2017-01-25 02:41:38 +0100

    bplist: Improve integer node parsing, remove unnecessary memcpy()

--- src/bplist.c
+++ src/bplist.c
@@ -214,19 +214,17 @@ static plist_t parse_uint_node(const char **bnode, uint8_t size)
     case sizeof(uint16_t):
     case sizeof(uint32_t):
     case sizeof(uint64_t):
-        memcpy(&data->intval, *bnode, size);
-        data->intval = UINT_TO_HOST(&data->intval, size);
         data->length = sizeof(uint64_t);
         break;
     case 16:
-        memcpy(&data->intval, (*bnode)+8, sizeof(uint64_t));
-        data->intval = UINT_TO_HOST(&data->intval, sizeof(uint64_t));
         data->length = size;
         break;
     default:
         free(data);
         return NULL;
     };
+
+    data->intval = UINT_TO_HOST(*bnode, size);
 
     (*bnode) += size;
     data->type = PLIST_UINT;
