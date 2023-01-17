85f5cbd3705b34fcc52009ca51d8167ab18764fa
Nikias Bassen <nikias@gmx.li>
2023-01-17 01:26:58 +0100

    oplist: Fix another OOB read
    Credit to OSS-Fuzz

--- src/oplist.c
+++ src/oplist.c
@@ -715,6 +715,13 @@ static int node_from_openstep(parse_ctx ctx, plist_t *plist)
                 plist_free_data(data);
                 goto err_out;
             }
+            if (ctx->pos >= ctx->end) {
+                byte_array_free(bytes);
+                plist_free_data(data);
+                PLIST_OSTEP_ERR("EOF while parsing data terminator '>' at offset %ld\n", ctx->pos - ctx->start);
+                ctx->err++;
+                goto err_out;
+            }
             if (*ctx->pos != '>') {
                 byte_array_free(bytes);
                 plist_free_data(data);
